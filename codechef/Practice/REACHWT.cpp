0ba237c34e96ab26b5547e996439ad38d6065bbb
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-02 20:01:45 +0530

    Solved: REACHWT (codechef)

---
+++ codechef/Practice/REACHWT.cpp
@@ -0,0 +1,8 @@
+// Problem: REACHWT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START254C/problems/REACHWT?tab=ide
+// Solved on: 2026-09-02T14:31:42.499Z
+
+// source not captured automatically - open the solution page and copy it manually
