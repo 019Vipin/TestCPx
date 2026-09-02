d3be884015eb8e7fa2c7426731b753c996bc72f2
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-02 20:11:30 +0530

    Solved: GOODSUBSETEZ (codechef)

---
+++ codechef/Practice/GOODSUBSETEZ.cpp
@@ -0,0 +1,8 @@
+// Problem: GOODSUBSETEZ
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START254C/problems/GOODSUBSETEZ?tab=ide
+// Solved on: 2026-09-02T14:41:28.135Z
+
+// source not captured automatically - open the solution page and copy it manually
