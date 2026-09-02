8eee21855f6e6c732f4320519593a2bdc2c798cd
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-02 20:07:59 +0530

    Solved: THREEGAME (codechef)

---
+++ codechef/Practice/THREEGAME.cpp
@@ -0,0 +1,8 @@
+// Problem: THREEGAME
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START254C/problems/THREEGAME?tab=ide
+// Solved on: 2026-09-02T14:37:56.174Z
+
+// source not captured automatically - open the solution page and copy it manually
