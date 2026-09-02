9cbe82197a40ddaa9d3b1f62a610d883375662c4
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-02 18:22:48 +0530

    Solved: Traffic Light (codeforces)

--- codeforces/1000-1099/1744C.cpp
+++ codeforces/1000-1099/1744C.cpp
@@ -1,10 +1,37 @@
-// Problem: 1744C - Traffic Light
+// Problem: Traffic Light
 // Platform: codeforces
-// Contest: Contest-1744
+// Contest: 1000
 // Rating/Difficulty: 1000
-// Language: C++20 (GCC 13-64)
+// Language: C++17
 // Verdict: Accepted
-// URL: https://codeforces.com/contest/1744/submission/1744
-// Solved on: 2026-09-02T12:50:41.322Z
+// URL: 
+// Solved on: 2026-09-02T12:52:45.285Z
 
-// source not captured automatically - open the submission page and copy it manually
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    char c;
+	    cin>>c;
+	    string s;
+	    cin>>s;
+	    string ss=s+s;
+	    int seen_green_idx=-1;
+	    int max_possible=INT_MIN;
+	    for(int i=2*n-1;i>=0;i--){
+	        if(ss[i]=='g'){
+	            seen_green_idx=i;
+	        } if(i<n && ss[i]==c){
+	            int req_sec=seen_green_idx-i;
+	            max_possible=max(max_possible,req_sec);
+	        }
+	    }
+	    cout<<max_possible<<endl;
+	}
+
+}
