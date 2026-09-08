56f729a57a225c611a6edb46f85d772b20eb28f7
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-08 23:39:26 +0530

    Solved: Count Commas in Range (leetcode)

--- leetcode/Easy/count-commas-in-range.cpp
+++ leetcode/Easy/count-commas-in-range.cpp
@@ -4,24 +4,13 @@
 // Language: cpp
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/count-commas-in-range/
-// Solved on: 2026-09-08T18:02:12.456Z
+// Solved on: 2026-09-08T18:09:22.296Z
 
-
-        class Solution{
-            public:
-                long long countCommas(long long n){
-                        long long commas=0;
-                                long long base=1000;
-                                        int commaCount=1;
-                                                while(base<=n){
-                                                            long long nextBase=base*1000;
-                                                                        long long end=min(n,nextBase-1);
-                                                                                    long long numbers=end-base+1;
-                                                                                                commas+=numbers*commaCount;
-                                                                                                            base=nextBase;
-                                                                                                                        commaCount++;
-                                                                                                                                }
-                                                                                                                                        return commas;
-                                                                                                                                            }
-                                                                                                                                            
-    };
+class Solution {
+public:
+    int countCommas(int n) {
+         if(n<1000)
+            return 0;
+        return n-999;
+    }
+};
