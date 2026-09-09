33c02c3141ec8fc852992dcbd85fdaf55445a213
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-09 20:12:47 +0530

    Solved: ADDIS (codechef)

---
+++ codechef/Practice/ADDIS.cpp
@@ -0,0 +1,8 @@
+// Problem: ADDIS
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255C/problems/ADDIS?tab=ide
+// Solved on: 2026-09-09T14:42:43.882Z
+
+// source not captured automatically - open the solution page and copy it manually
