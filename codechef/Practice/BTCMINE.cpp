eeec43ffa603841818a21b3979069267ffd71064
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-09-09 20:10:18 +0530

    Solved: BTCMINE (codechef)

---
+++ codechef/Practice/BTCMINE.cpp
@@ -0,0 +1,8 @@
+// Problem: BTCMINE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255C/problems/BTCMINE?tab=ide
+// Solved on: 2026-09-09T14:40:14.995Z
+
+// source not captured automatically - open the solution page and copy it manually
